a343a81662c1ed57b864c4ee57494ae2a90e10f2
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 13:58:49 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,12 +1,10 @@
 function IcebreakerResponse(email){
   this.email = email
-
-  IcebreakerResponse.BatchCreate = function (emails) {
-    return emails.map(function (emailData) {
-      return new IcebreakerResponse(emailData);
-      
-    })
-    
-  }
 }
 
+IcebreakerResponse.BatchCreate = function(emails) {
+  return emails.map(function (emailData) {
+    return new IcebreakerResponse(emailData);
+  })
+
+}
