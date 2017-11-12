6016352e17e8a2227837a06eb53a708e95dc0574
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 13:59:14 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails) {
+  return emails.map(function (emailData) {
+    return new IcebreakerResponse(emailData);
+  })
+
+}
