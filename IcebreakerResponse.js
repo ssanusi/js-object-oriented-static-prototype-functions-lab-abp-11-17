626bf86c1632d196c1933cd9d3defb921dc86238
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 13:55:40 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,12 @@
 function IcebreakerResponse(email){
   this.email = email
+
+  IcebreakerResponse.BatchCreate = function (emails) {
+    return emails.map(function (emailData) {
+      return new IcebreakerResponse(emailData);
+      
+    })
+    
+  }
 }
 
